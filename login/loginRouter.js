129ac71d1d49441fcc907af343729d7fc26b1ef2
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-17 18:05:37 -0700

    Verify passwords using bcryptjs

--- login/loginRouter.js
+++ login/loginRouter.js
@@ -1,9 +1,21 @@
-const { route } = require("../users/usersRouter");
-
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const Users = require("../users/usersModel");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Login router is working!" });
+router.post("/", (req, res) => {
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${username}` });
+      } else {
+        res.status(401).json({ error: "Sorry, wrong username or password." });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
 });
 
 module.exports = router;
