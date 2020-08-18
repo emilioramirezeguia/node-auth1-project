5825be969687302449d8a239f201fe77c5b32d7d
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-17 19:06:24 -0700

    Destroy session on logout

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
+// Import npm packages
 const express = require("express");
+const session = require("express-session");
+
+// Import routers && middleware
 const usersRouter = require("./users/usersRouter");
-const registerRouter = require("./register/registerRouter");
-const loginRouter = require("./login/loginRouter");
+const authRouter = require("./auth/authRouter");
+const protected = require("./auth/protectedMW");
 
 const server = express();
+const sessionConfig = {
+  name: "monster",
+  secret: "keep it secret, keep it safe",
+  cookie: {
+    maxAge: 1000 * 60 * 10, // session expires after 10 minutes
+    secure: false, // if true cookie is only sent over https
+    httpOnly: true, // JavaScript can't touch cookie
+  },
+  resave: false,
+  saveUninitialized: false, // GDPR Compliance, the client should drive this
+};
+
+// Use middleware
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.get("/api", (req, res) => {
   res.status(200).json({ message: "It's working... it's working!" });
 });
 
-server.use("/api/users", usersRouter);
+server.use("/api/users", protected, usersRouter);
 
-server.use("/api/register", registerRouter);
-server.use("/api/login", loginRouter);
+server.use("/api/auth", authRouter);
 
 const port = 8000;
 server.listen(port, () => {
